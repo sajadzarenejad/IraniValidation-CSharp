52113320c536f214df521848d68719afb7b766af
sajadzarenejad <sajadzarenejad@gmail.com>
2019-04-05 14:27:58 +0430

    add mobile oprator regular expression

---
+++ IraniValidator/PhoneValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace IraniValidator
+{
+    public static class PhoneValidator
+    {
+        private static readonly string HamrahAvalMobileExpression = "";
+        private static readonly string IranCellAvalMobileExpression = "";
+        private static readonly string RightelMobileExpression = "";
+    }
+
+
+}
---
+++ IraniValidator/ReqularExpressionFactory.cs
@@ -0,0 +1,83 @@
+namespace IraniValidator
+{
+    internal class ReqularExpressionFactory
+    {
+        public static string HamrahAvalExpressionMaker(PhoneFormatType phoneFormat)
+        {
+            switch (phoneFormat)
+            {
+                default : case PhoneFormatType.StartWithZero :
+                    return "09(1[0-9]|90)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWithPlus98:
+                    return "+989(1[0-9]|90)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith98:
+                    return "989(1[0-9]|90)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith9:
+                    return "9(1[0-9]|90)-?[0-9]{3}-?[0-9]{4}";
+
+            }
+        }
+
+        public static string IrancellExpressionMaker(PhoneFormatType phoneFormat)
+        {
+            switch (phoneFormat)
+            {
+                default:
+                case PhoneFormatType.StartWithZero:
+                    return "09(01|02|03|05|30|35|36|37|38|39)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWithPlus98:
+                    return "+989(01|02|03|05|30|35|36|37|38|39)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith98:
+                    return "989(01|02|03|05|30|35|36|37|38|39)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith9:
+                    return "9(01|02|03|05|30|35|36|37|38|39)-?[0-9]{3}-?[0-9]{4}";
+
+            }
+        }
+
+        public static string RightelExpressionMaker(PhoneFormatType phoneFormat)
+        {
+            switch (phoneFormat)
+            {
+                default:
+                case PhoneFormatType.StartWithZero:
+                    return "09(20|21|22)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWithPlus98:
+                    return "+989(20|21|22)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith98:
+                    return "989(20|21|22)-?[0-9]{3}-?[0-9]{4}";
+                case PhoneFormatType.StartWith9:
+                    return "9(20|21|22)-?[0-9]{3}-?[0-9]{4}";
+
+            }
+        }
+    }
+
+    public enum PhoneFormatType
+    {
+        //example 09198765432
+        //this is the default format
+        StartWithZero = 1,
+
+        //example +989198765432
+        StartWithPlus98 = 2,
+
+        //example 989198765432
+        StartWith98 = 3,
+
+        //example 9198765432
+        StartWith9 = 4
+
+    }
+
+
+    /// <summary>
+    /// Choose proper oprator
+    /// </summary>
+    public enum OpratorType{
+        AllOpprator=0,
+        HamrahAval=1,
+        IranCell=2,
+        Rightel=3
+    }
+}
